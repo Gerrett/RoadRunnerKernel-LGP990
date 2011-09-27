1fa11f1838cb6c7b9988174229808a6ac5166528
Gerrett <Gerrett84@gmail.com>
2011-09-27 16:19:55 +0200

    test sdram hack 2

--- arch/arm/mach-tegra/odm_kit/star/query/nvodm_query.c
+++ arch/arm/mach-tegra/odm_kit/star/query/nvodm_query.c
@@ -347,8 +347,8 @@ static const NvOdmSdramControllerConfigAdv s_NvOdmStarSmartphoneHynixEmcConfigTa
     },
     {
                   0x20,   /* Rev 2.0 */
-         ,      350000,   /* SDRAM frquency */
-                  1250,   /* EMC core voltage */
+                350000,   /* SDRAM frquency */
+                  1200,   /* EMC core voltage */
                     46,   /* Number of EMC parameters below */
         {
             0x00000012,   /* RC */
@@ -676,7 +676,7 @@ static const NvOdmSdramControllerConfigAdv s_NvOdmStarSmartphoneHynixEmcConfigTa
     {
                   0x20,   /* Rev 2.0 */
                 350000,   /* SDRAM frquency */
-                  1250,   /* EMC core voltage */
+                  1200,   /* EMC core voltage */
                     46,   /* Number of EMC parameters below */
         {
 					  0x00000012,	/* RC */
